1424d09f65291544e70192bfd2317fbeba1af1b7
AngelaB1041 <abarsallo@smu.edu>
2018-11-15 17:46:18 -0600

    Added all the functions given to the AVL tree

--- Sprint5/searchEngine/thanosTree.h
+++ Sprint5/searchEngine/thanosTree.h
@@ -9,6 +9,8 @@
  * Modified: 13 November 2018
  * Added templating to the existing code
  *
+ * Modified: 15 November 2018
+ * Finished adding all of the functions Weiss implemented on the tree
 */
 
 #ifndef THANOSTREE_H
@@ -32,7 +34,7 @@ private:
                     thanosNode(const T& theElement, thanosNode *lt, thanosNode *rt, int h = 0) : element(theElement), left(lt), right(rt), height(h){}
             };
 
-            thanosNode *root;
+            thanosNode* root;
 
             int height(thanosNode *t) const;
             int max(int lhs, int rhs) const;
@@ -41,6 +43,13 @@ private:
             void rotateWithRightChild(thanosNode* & k2);
             void doubleWithLeftChild(thanosNode* & k3);
             void doubleWithRightChild(thanosNode* & k1);
+            thanosNode* findMin(thanosNode *t) const;
+            thanosNode* findMax(thanosNode *t) const;
+            bool contains(const T& x, thanosNode* t) const;
+            void makeEmpty(thanosNode* &t);
+            void printTree(thanosNode* t) const;
+            thanosNode* clone(thanosNode* t) const;
+
 
 public:
             thanosTree() : root(NULL){}
@@ -52,8 +61,14 @@ public:
             bool isEmpty() const;
             void makeEmpty();
             void insert(const int &x);
+            const thanosTree& operator=(const thanosTree& rhs);
 
 };
+
+template<class T>
+thanosTree<T>::thanosTree(const thanosTree &rhs) : root(NULL){
+    *this = rhs;
+}
 
 /***
  * Return the height of the node t or -1 if NULL
@@ -75,43 +90,41 @@ int thanosTree<T>::max(int lhs, int rhs) const{
  * Set the new root of the subtree
  **/
 template<class T>
-void thanosTree<T>::insert(const T &x, thanosNode* &t){
-
+void thanosTree<T>::insert(const T& x, thanosNode* &t){
+       if (t==NULL)
+           t = new thanosNode(x, NULL, NULL);
+       else if(x < t->element){
+           insert(x, t->left);
+           if(height(t->left) - height(t->right)==2)
+               if(x < t->left->element)
+                   rotateWithLeftChild(t);
+                else
+                   doubleWithLeftChild(t);
+       }
+       else if(t->element < x){
+           insert(x, t->right);
+           if(height(t->right) - height(t->left) == 2)
+               if(t->right->element < x)
+                   rotateWithRightChild(t);
+                else
+                   doubleWithRightChild(t);
+       }
+       else
+           ;
+       t->height = max(height(t->left), height(t->right)) + 1;
 }
 
-/***
- * Find minimum item in the tree. throw underflowexveption if empty
- **/
+/*
+ *Internal method to find the smallest item in a subtree t
+ * Return node containing smallest item.
+ */
 template<class T>
-const int& thanosTree<T>::findMin() const{
-    if(isEmpty())
-    {
-        throw UnderflowException();
-    }
-    return findMin(root)->element;
-}
-
-/***
- * Find the largest item in the tree.
- * Throw UnderflowException if empty
- **/
-template<class T>
-const int &findMax() const{
-    if(isEmpty())
-    {
-        throw UnderflowException();
-    }
-    return (findMax(root)->element);
-}
-
-template<class T>
-thanosTree<T>::thanosTree(const thanosTree &rhs) : root(NULL){
-    *this = rhs;
-}
-
-template<class T>
-thanosTree<T>::~thanosTree(){
-    makeEmpty();
+thanosNode* thanosTree<T>::findMin(thanosNode* t) const{
+    if(t==NULL)
+        return NULL;
+    if(t->left==NULL)
+        return t;
+    return findMin(t->left);
 }
 
 /***
@@ -126,6 +139,18 @@ const T& thanosTree<T>::findMin() const{
     return findMin(root)->element;
 }
 
+/*
+ * Internal Method to find the largest item in a subtree t.
+ * Return node containing the smallest item
+ */
+template<class T>
+thanosNode* thanosTree<T>::findMax(thanosNode *t) const{
+    if(t!=NULL)
+        while(t->right != NULL)
+            t = t->right;
+    return t;
+}
+
 /***
  * Find the largest item in the tree.
  * Throw UnderflowException if empty
@@ -135,9 +160,42 @@ const T& thanosTree<T>::findMax() const{
     if(isEmpty())
     {
         throw UnderflowException();
-
     }
     return (findMax(root)->element);
+}
+
+/*
+ * Internal method to test if an item is in a subtree
+ * x is item to search for
+ * t is the node that roots the tree
+ */
+bool thanosTree<T>::contains(const T &x, thanosNode *t) const{
+    if(t==NULL)
+        return false;
+    else if(x < t->element)
+        return contains(x, t->left);
+    else if(t->element < x)
+        return contains(x, t->right);
+    else
+        return true;
+}
+
+/*
+ * Internal method to make subtree empty
+ */
+template<class T>
+void thanosTree<T>::makeEmpty(thanosNode* &t){
+    if(t != NULL){
+        makeEmpty(t->left);
+        makeEmpty(t->right);
+        delete t;
+    }
+    t = NULL;
+}
+
+template<class T>
+thanosTree<T>::~thanosTree(){
+    makeEmpty();
 }
 
 /***
@@ -172,6 +230,36 @@ void thanosTree<T>::insert(const int &x){
     insert(x, root);
 }
 
+template<class T>
+void thanosTree<T>::rotateWithLeftChild(thanosNode *&k2){
+    thanosNode* k1 = k2->left;
+    k2->left = k1->right;
+    k1->right = k2;
+    k2->height = max(height(k2->left), height(k2->right)) + 1;
+    k1->height = max(height(k1->left), k2->height) + 1;
+    k2 = k1;
+}
 
+template<class T>
+void thanosTree<T>::rotateWithRightChild(thanosNode *&k1){
+    thanosNode* k2 = k1->right;
+    k1->right = k2->left;
+    k2->left = k1;
+    k1->height = max(height(k1->left), height(k1->right)) + 1;
+    k2->height = max(height(k2->right), k1->height) + 1;
+    k1 = k2;
+}
+
+template<class T>
+void thanosTree<T>::doubleWithLeftChild(thanosNode *&k3){
+    rotateWithRightChild(k3->left);
+    rotateWithLeftChild(k3);
+}
+
+template<class T>
+void thanosTree<T>::doubleWithRightChild(thanosNode *&k1){
+    rotateWithLeftChild(k1->right);
+    rotateWithRightChild(k1);
+}
 
 #endif // AVLTREE_H
