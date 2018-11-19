7e05a43170ed4539a2269042b93b6b545692c9ab
Chelby <cjrhoades@smu.edu>
2018-11-18 23:19:27 -0600

    WOW I DID IT STEMMING AND STOPPING THE DATA

--- Sprint5/searchEngine/parser.cpp
+++ Sprint5/searchEngine/parser.cpp
@@ -3,9 +3,12 @@
  * Modified: November 18, 2018
  */
 #include "parser.h"
+#include "stopnstem.h"
 #include <iostream>
 #include <myhtml/api.h>
 #include <fstream>
+#include <utility>
+#include <vector>
 using std::string;
 using std::cout;
 using std::endl;
@@ -20,8 +23,14 @@ parser::~parser()
 
 void parser::goThru(string fileName, string word)
 {
+    /*initialize le variables*/
+    int numWords, numFiles;
     string tmpString, longstring, htmlText;
     string lookingFor = "plain_text";
+    stopNstem SNS;
+
+
+    /*open the file yall*/
     inFile.open(fileName);
     specialWord = word; //this is the one we are looking for!
    if(inFile.is_open())
@@ -33,16 +42,32 @@ void parser::goThru(string fileName, string word)
            {
            //.. found.
                getline(inFile, htmlText);
-               longstring = htmlText;
+               cout << htmlText;
+
                while(!inFile.eof())
                {
-                   getline(inFile, htmlText);
+                inFile >> longstring;
+                bool checker = SNS.checkStop(longstring);
+                if(checker == false)
+                {
+                    //check if in stemmer and stem her
+                    bool checkStem = SNS.seeIfInStems(longstring);
+                    /*HERE SHE IS*/
 
-                   longstring = longstring + htmlText;   //adds everything until the end of the file to the string
-               }//end while
+                }else{
+                    //she's a stop word.Leave her be.
+                }//end else
+
+            }//end while
+
+
+
+
            }//end if
-        cout << longstring;
        }//end while
+       cout << tmpString;
+
+       /*Here is where we put the build*/
         inFile.close();  //no memory leaks today
    }else{
        cout << "oh no look at that I couldn't open this file. Try again." << endl;
